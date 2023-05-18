e1247c62f1255ac577e422e60d205275eda02f6e
Chris S <chris@soundshare.co.uk>
2023-05-18 17:49:25 +0100

    example of issue

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,17 @@
-FROM ghcr.io/codewars/purescript:latest
+FROM ghcr.io/codewars/purescript:0.15.4.2
+
+WORKDIR /myapp
+
+USER root
+RUN chown codewarrior:codewarrior -R /myapp
+
+USER codewarrior
 
 RUN npm install purescript
 RUN npm install spago
+
+ADD packages.dhall spago.dhall /myapp/
+ADD ./src /myapp/src/
+
+RUN spago build
+RUN spago run
--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,25 @@
 module Main where
 
-import Prelude
+import Prelude (Unit, discard, identity)
+
 
 import Effect (Effect)
 import Effect.Console (log)
 
+import Halogen.HTML.Elements
+import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
+import Halogen.VDom.DOM.StringRenderer (render)
+
 main :: Effect Unit
 main = do
-  log "🍝"
+  log "begin"
+  let x = renderTheHtml example
+  log x
+  log "done"
+
+renderTheHtml :: forall i4. HH.HTML String i4 -> String
+renderTheHtml (HH.HTML v) = render identity v
+
+example :: forall w11 i12. HH.HTML w11 i12
+example = p [HP.tabIndex 0] []
